3789a5628ad5d8b0649606b9e2a90ce26faa6147
Ramon Barros <ramonfilipe@gmail.com>
2021-04-12 15:32:37 -0300

    criação do projeto

---
+++ clientes/src/test/java/com/ramon/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramon.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
